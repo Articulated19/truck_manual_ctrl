e1343c4b113a871e5c6ea7baa42d07ffbc39b7dc
seholm <filipslottner@gmail.com>
2017-02-15 21:04:19 +0100

    added test stub

---
+++ script/converter_test.py
@@ -0,0 +1,12 @@
+import unittest
+#from converter import *
+
+class TestConverter(unittest.TestCase):
+    def test_convert_method1(self):
+        self.assertTrue(True)
+
+    def test_convert_method2(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
